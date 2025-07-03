8152a3916e0abb82e89cce4e4dbb86e5015262ce
etoundijuniorpierre <etoundijunior@hotmail.com>
2025-07-03 21:16:41 +0100

    fix : mapping

--- package-service/src/main/java/com/logisticsCompany/mapper/PackageMapper.java
+++ package-service/src/main/java/com/logisticsCompany/mapper/PackageMapper.java
@@ -4,13 +4,9 @@ import com.logisticsCompany.dto.PackageResponseDto;
 import com.logisticsCompany.dto.PackageRequestDto;
 import com.logisticsCompany.dto.microServiceDto.LocationReponseDto;
 import com.logisticsCompany.entities.PackageEntity;
-import com.logisticsCompany.entities.enums.Status;
 import com.logisticsCompany.feign.LocationClient;
-import com.logisticsCompany.repository.PackageRepository;
-import org.bouncycastle.util.Pack;
 import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -20,9 +16,6 @@ public abstract class PackageMapper {
 
     @Autowired
     private LocationClient locationClient;
-
-     @Autowired
-     private PackageRepository packageRepository;
 
     @Mapping(target = "locationId", source = "location")
     public abstract PackageEntity toEntity(PackageRequestDto packageRequestDto);
@@ -37,15 +30,8 @@ public abstract class PackageMapper {
     public abstract void updateEntityFromDto(PackageRequestDto packageRequestDto, @MappingTarget PackageEntity packageEntity);
 
     @Named("packageWithLocation")
-    public LocationReponseDto packageWithLocation(Long id, @MappingTarget PackageEntity packageEntity) {
-        ResponseEntity<LocationReponseDto> response = locationClient.getLocationById(String.valueOf(id));
-        LocationReponseDto reponseDto = response.getBody();
-
-        if (reponseDto.isCheckpointAvailable()) {
-            packageEntity.setStatus(Status.IN_TRANSIT);
-        }
-
-        return reponseDto;
+    LocationReponseDto packageWithLocation(Long id) {
+        return locationClient.getLocationById(String.valueOf(id)).getBody();
     }
 
 }
